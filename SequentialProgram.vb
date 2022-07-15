c6568d9dd356c87de66b9711117da2b373950ed3
Peter <BraniaP@tisoblackstar.co.za>
2022-07-15 14:18:19 +0200

    Hardcoded path

--- SequentialProgram.vb
+++ SequentialProgram.vb
@@ -363,11 +363,11 @@ Public Class SequentialWorkflow
             Utils.LogError("Please define the 'imgFolderPath' parameter")
             Return
         End If
-        Dim imgPhysicalPath = Context.ParValue("imgPhysicalPath")
-        If imgPhysicalPath = "" Then
-            Utils.LogError("Please define the 'imgPhysicalPath' parameter")
-            Return
-        End If
+        'Dim imgPhysicalPath = Context.ParValue("imgPhysicalPath")
+        'If imgPhysicalPath = "" Then
+        '    Utils.LogError("Please define the 'imgPhysicalPath' parameter")
+        '    Return
+        'End If
 
         'process the file one by one
         Dim dataList As List(Of TeraDP.GN4.Workflow.IActivityData) = New List(Of TeraDP.GN4.Workflow.IActivityData)(Context.Data)
@@ -453,6 +453,7 @@ Public Class SequentialWorkflow
                         createImgs.Overwrite = True
                         Dim createImgsRes As ImportXmlResult = createImgs.Do()
 
+                        Dim imgPhysicalPath = "\\AMENDOORIS02\gn4VALIDATIONWORKFLOW$\GN4_BW_Puzzles_Input\BT\Puzzles"
                         Xml_Transform.UploadPdf(imgPhysicalPath, "puzzle_" & crossWordDateStr, xmlSourceTransformed.XPathSelectElement("//images/default").Value)
                         Xml_Transform.UploadPdf(imgPhysicalPath, "solution_" & imgSolutionDateStr, xmlSourceTransformed.XPathSelectElement("//images/solution").Value())
 
